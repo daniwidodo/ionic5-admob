c01fc274e3c89600bae16777db62777340d6e6ff
bangtur_studio <dani.widodo87@gmail.com>
2021-04-29 02:20:07 +0800

    Initial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+import { AdOptions, AdSize, AdPosition } from '@capacitor-community/admob';
+const { AdMob } = Plugins;
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  private options: AdOptions = {
+    adId: 'ca-app-pub-3940256099942544/6300978111',
+    adSize: AdSize.BANNER,
+    position: AdPosition.BOTTOM_CENTER,
+    margin: 0,
+    isTesting: true
+    // npa: true
+  };
 
-  constructor() {}
+  constructor() {
+        // Show Banner Ad
+        AdMob.showBanner(this.options);
+
+        // Subscribe Banner Event Listener
+        AdMob.addListener('onAdLoaded', (info: boolean) => {
+          console.log('Banner Ad Loaded');
+        });
+    
+        // Get Banner Size
+        AdMob.addListener('onAdSize', (info: boolean) => {
+          console.log(info);
+        });
+  }
 
 }
